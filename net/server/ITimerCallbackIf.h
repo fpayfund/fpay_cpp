b34fa6ace43b63e85e2e09f0f21914c95bdd9b3b
wisefan <wisefan@fpay.io>
2018-07-04 17:28:31 +0800

    interface modify

--- net/server/ITimerCallbackIf.h
+++ net/server/ITimerCallbackIf.h
@@ -4,12 +4,14 @@
 //上层模块实现该接口(定时器接口）
 struct ITimerCallbackIf {
   	//根节点切换时机检测定时器
-    virtual void onRootSwitchCheck() = 0;
-	//区块同步是否完成检查定时器（检测区块是否同步完成，没有同步完成，上层继续发送同步请求）
-	virtual void onBlockSyncCheck() = 0;
+    virtual void onTimerRootSwitchCheck() = 0;
+	//区块完整性检查定时器（检测区块是否同步完成，没有同步完成，上层继续发送同步请求）
+	virtual void onTimerBlocksFullCheck() = 0;
 	//打包时机检测定时器（检测是不是应该开始打包，如果是，则打包区块）
-	virtual void onProduceBlockCheck() = 0;
+	virtual void onTimerProduceBlockCheck() = 0;
 
+	//定时检测是否有更好的路由（更好的父节点）
+	virtual void onTimerBestRouteCheck() = 0;
 };
 
 #endif
