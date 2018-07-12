6db2ea093a9b5efa11e5d05b501850f551d160dd
wisefan <wisefan@fpay.io>
2018-07-12 18:33:14 +0800

    add by wisefan

---
+++ net/client/IClientCallbackIf.h
@@ -0,0 +1,22 @@
+#ifndef FPAY_CLIENT_CALLBACK_IF_H_
+#define FPAY_CLIENT_CALLBACK_IF_H_
+#include "protocol/fpay_protocol.h"
+using namespace ::fpay::protocol;
+
+//上层模块实现该接口
+struct IClientCallbackIf {
+	
+	//子节点注册事件回应
+    virtual int onReceiveChildRegisterRes(const NodeRegisterRes& reg) = 0;
+	//父节点连接断开事件
+    virtual void onReceiveParentLeave(Byte32& child_address) = 0;
+	//受理支付请求回应
+	virtual int onReceivePayRes(const PayRes& res) = 0;
+    //矿工节点的同步区块请求回应
+	virtual int onReceiveSyncBlocksRes(const SyncBlocksRes& res) = 0;
+    //查询亲属节点信息回应
+	virtual int onReceiveGetRelativesRes(const GetRelativesRes& res) = 0;
+};
+
+#endif
+
---
+++ net/server/IServerTimerIf.h
@@ -0,0 +1,18 @@
+#ifndef FPAY_TIMER_CALLBACK_IF_H_
+#define FPAY_TIMER_CALLBACK_IF_H_
+
+//上层模块实现该接口(定时器接口）
+struct IServerTimerIf {
+  	//根节点切换时机检测定时器
+    virtual void onTimerRootSwitchCheck() = 0;
+	//区块完整性检查定时器（检测区块是否同步完成，没有同步完成，上层继续发送同步请求）
+	virtual void onTimerBlocksFullCheck() = 0;
+	//打包时机检测定时器（检测是不是应该开始打包，如果是，则打包区块）
+	virtual void onTimerProduceBlockCheck() = 0;
+
+	//定时检测是否有更好的路由（更好的父节点）
+	virtual void onTimerBestRouteCheck() = 0;
+};
+
+#endif
+
