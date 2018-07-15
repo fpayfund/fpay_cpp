37b944b65aafa5bcbed613d99cb825ba87ef357f
wisefan <wisefan@fpay.io>
2018-07-15 21:40:15 +0800

    modify by wisefan

---
+++ net/client/ClientConst.h
@@ -0,0 +1,13 @@
+#ifndef FPAY_CLIENT_CONST_H_
+#define FPAY_CLIENT_CONST_H_
+#include <string>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+//注册到网络完成
+#define BIT_CLIENT_INIT_REGISTER_OVER 0x0000000000000001
+//区块同步完成
+#define BIT_CLIENT_INIT_BLOCKS_FULL 0x0000000000000002
+
+
