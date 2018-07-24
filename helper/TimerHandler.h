df0eb9fe19c63f6903afca876c0753ab089a2a38
wisefan <wisefan@fpay.io>
2018-07-24 10:45:49 +0800

    add by wisefan

---
+++ helper/TimerHandler.h
@@ -0,0 +1,52 @@
+#ifndef __FPAY_TIME_HANDLER_H__
+#define __FPAY_TIME_HANDLER_H__
+#include "core/sox/snox.h"
+
+template <typename T>
+struct bind_func
+{
+
+		typedef bool (T::*callback)();
+};
+
+
+template<typename T, typename bind_func<T>::callback ptr>
+class TimerHandler : public sox::Handler
+{
+public:
+		TimerHandler()
+		{
+				_x = NULL;
+				_to = 0;
+		}
+		TimerHandler(T* x){
+				_x = x;
+				_to = 0;
+		}
+		void init(T* x)
+		{
+				_x = x;
+		}
+		void start(uint32_t timeout)
+		{
+				_to = timeout;
+				select_timeout(_to);
+		}
+		void stop()
+		{
+				select_timeout();
+		}
+		void handle(int /*ev*/)
+		{
+				if(_x) 
+				{		
+						if((_x->*ptr)()) select_timeout(_to);
+				}
+		}
+protected:
+		T* _x;
+		uint32_t _to;
+};
+
+#endif //__NU_TIME_HANDLER_H__
+
